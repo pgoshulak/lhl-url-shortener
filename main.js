ba390649a8f6cbe80901a798fed8ce0d3b770324
Peter Goshulak <pgoshulak@gmail.com>
2018-04-17 14:22:39 -0400

    Add POST method from form, using random base-62 string

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var express = require("express");
 var app = express();
 const bodyParser = require('body-parser');
+const base62 = require('base62-random');
 var PORT = process.env.PORT || 8080; // default port 8080
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return base62(8);
+}
 
 app.set('view engine', 'ejs');
 
@@ -21,12 +26,21 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
     shortUrl: req.params.id,
     urlData: urlDatabase[req.params.id]
   };
   res.render("url_show", templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send(`${generateRandomString()} :: ${req.body.longURL}`);
 });
 
 app.listen(PORT, () => {
