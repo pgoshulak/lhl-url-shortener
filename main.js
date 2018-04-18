a5639f0af1541e7ce32df32ad9fe816cea9c2aad
Peter Goshulak <pgoshulak@gmail.com>
2018-04-18 12:15:35 -0400

    Add main.css using /public/ folder

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
   res.redirect('urls');
 });
