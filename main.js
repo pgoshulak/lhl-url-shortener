0195fc575b306c9b42a1032f361880f2dcdf21f5
Peter Goshulak <pgoshulak@gmail.com>
2018-04-18 14:35:36 -0400

    Fix cookie-parser implementation

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const base62 = require('base62-random');
 var PORT = process.env.PORT || 8080; // default port 8080
 
@@ -17,6 +18,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(cookieParser());
+
 app.use(express.static('public'))
 
 app.get("/", (req, res) => {
@@ -26,7 +29,7 @@ app.get("/", (req, res) => {
 // URL index
 app.get("/urls", (req, res) => {
   let templateVars = { 
-    username: req.cookies('username'),
+    username: req.cookies['username'],
     urls: urlDatabase
   };
   res.render('urls_index', templateVars);
@@ -35,7 +38,7 @@ app.get("/urls", (req, res) => {
 // Page to add a new URL
 app.get("/urls/new", (req, res) => {
   let templateVars = { 
-    username: req.cookies('username')
+    username: req.cookies['username']
   };
   res.render("urls_new", templateVars);
 });
@@ -43,7 +46,7 @@ app.get("/urls/new", (req, res) => {
 // View the URL info
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
-    username: req.cookies('username'),
+    username: req.cookies['username'],
     shortUrl: req.params.id,
     urlData: urlDatabase[req.params.id]
   };
@@ -86,10 +89,17 @@ app.post('/urls/:id/delete', (req, res) => {
   res.redirect('/urls')
 });
 
+// Login and assign cookie as username
 app.post('/login', (req, res) => {
   let username = req.body.username;
   res.cookie('username', username);
-  res.redirect('/urls')
+  res.redirect('/urls');
+});
+
+// Logout and clear username cookie
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
