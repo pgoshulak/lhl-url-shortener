da647826fdf95358daebbdf3d7e6cdbfe6dc4ca0
Peter Goshulak <pgoshulak@gmail.com>
2018-04-20 13:26:37 -0400

    Fix bug where visitor_id cookie cleared after logout

--- main.js
+++ main.js
@@ -306,7 +306,7 @@ app.post('/login', (req, res) => {
 
 // Logout and clear username cookie
 app.post('/logout', (req, res) => {
-  req.session = null;
+  req.session.user_id = null;
   res.redirect('/');
   return;
 });
